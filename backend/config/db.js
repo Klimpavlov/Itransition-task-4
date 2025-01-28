bdd27d471f199de3efe48fa20fc9258a13bf5b09
Klimpavlov <Klimpavlov2002@gmail.com>
2025-01-28 12:40:38 +0300

    deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 
+// const db = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+// });
+
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
 });
 
 module.exports = db;
