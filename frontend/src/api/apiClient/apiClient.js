f09c1c441c7a2dc63de23c7530c39e638528da75
Klimpavlov <Klimpavlov2002@gmail.com>
2025-01-28 23:59:32 +0300

    blocked user auto logout

--- frontend/src/api/apiClient/apiClient.js
+++ frontend/src/api/apiClient/apiClient.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    // baseURL: 'https://itransition-task-4-hgpi.onrender.com',
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://itransition-task-4-hgpi.onrender.com',
     headers: {
         'Content-Type': 'application/json'
     }
